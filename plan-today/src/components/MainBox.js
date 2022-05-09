7870ca0f5b1a095dcfacf0d2ae5586e206214b49
kyw0716 <kyw0716@naver.com>
2022-05-09 12:16:41 +0900

    edit : using localStorage, delete
    todos local storage 저장 방식 변경:
    모든 todo들 따로 localStorage에 저장(기존 방식) => todos 배열에 json형식으로 각각의 todo들을 저장하고 todos 배열을 한꺼번에 localStorage에 "todos"라는 키 값의 value로 저장(변경된 방식)
    delete 방식 변경:
    각각 따로 저장된 todo 값을 localStorage에서 찾아 제거(기존 방식) => todos 배열에서 todo와 같은 name을 갖는 인자를 찾아 filter 함수를 이용해 제거

--- plan-today/src/components/MainBox.js
+++ plan-today/src/components/MainBox.js
@@ -2,76 +2,34 @@ import React, {useState, useEffect} from 'react';
 import Todo from "./Todo";
 import styles from "../css/MainBox.module.css";
 
-let j = 0;
-
 function MainBox(){
     const [inputData, setInputData] = useState("");
     const [submitData, setSubmitData] = useState("");
-    const [lsLength, setLsLength] = useState(0);
-    const [todos, setTodos] = useState([]);
     const [show, setShow] = useState(0);
-    const [checked, setChecked] = useState(false);
-    let TODO = document.getElementsByClassName('todo');
+    const [todos, setTodos] = useState(()=>{
+        const savedTodos = localStorage.getItem("todos");
 
-    useEffect(()=>{
-        setTodos([]);
-        for(let i = 0; i < localStorage.length; i++){
-            setTodos(current => [localStorage.getItem(localStorage.key(i)), ...current]);
+        if(savedTodos){
+            return JSON.parse(savedTodos);
         }
-    },[lsLength]);
+        else{
+            return [];
+        }
+    });
+    
+    useEffect(()=>{
+        localStorage.setItem("todos", JSON.stringify(todos));
+    },[todos]);
 
     useEffect(()=>{
         if(submitData !== ""){
-            setLsLength(current => current + 1);
-            localStorage.setItem(submitData,submitData);
-            setTodos(current => [localStorage.getItem(submitData), ...current]);
+            setTodos([
+                ...todos, {
+                    id : todos.length + 1, todo : submitData, checked : false
+                }
+            ]);
         }
     },[submitData]);
-
-    useEffect(()=>{
-        j += 1;
-        if(j > 2){
-            if(show === -1){
-                for(let i = 0; i < localStorage.length; i++){
-                    if(TODO[i].firstChild.firstChild.checked === true){
-                        TODO[i].style.display = "none";
-                    }
-                    else if(TODO[i].firstChild.firstChild.checked === false){
-                        TODO[i].style.display = "";
-                    }
-                }
-                document.getElementById("ing").style.color = "rgb(146, 121, 115)";
-                document.getElementById("today").style.color = "rgb(180, 156, 150)";
-                document.getElementById("done").style.color = "rgb(180, 156, 150)";
-            }
-            else if(show === 0){
-                for(let i = 0; i < localStorage.length; i++){
-                    if(TODO[i].firstChild.firstChild.checked === true){
-                        TODO[i].style.display = "";
-                    }
-                    else if(TODO[i].firstChild.firstChild.checked === false){
-                        TODO[i].style.display = "";
-                    }
-                }
-                document.getElementById("ing").style.color = "rgb(180, 156, 150)";
-                document.getElementById("today").style.color = "rgb(146, 121, 115)";
-                document.getElementById("done").style.color = "rgb(180, 156, 150)";
-            }
-            else if(show === 1){
-                for(let i = 0; i < localStorage.length; i++){
-                    if(TODO[i].firstChild.firstChild.checked === true){
-                        TODO[i].style.display = "";
-                    }
-                    else if(TODO[i].firstChild.firstChild.checked === false){
-                        TODO[i].style.display = "none";
-                    }
-                }
-                document.getElementById("ing").style.color = "rgb(180, 156, 150)";
-                document.getElementById("today").style.color = "rgb(180, 156, 150)";
-                document.getElementById("done").style.color = "rgb(146, 121, 115)";
-            }
-        }
-    },[show, checked]);
 
     const inputOnChange = (e) => {
         setInputData(e.target.value);
@@ -82,15 +40,19 @@ function MainBox(){
         setInputData("");
     }
     const onClick = (e) => {
-        if(e.target.innerHTML === "진행중"){
+        if(e.target.name === "doing"){
             setShow(-1);
         }
-        else if(e.target.innerHTML === "오늘"){
+        else if(e.target.name === "all"){
             setShow(0);
         }
-        else if(e.target.innerHTML === "완료"){
+        else if(e.target.name === "done"){
             setShow(1);
         }
+    }
+
+    const handleDelete = (e) => {
+        setTodos(todos.filter(current => current.todo !== e.target.name));
     }
 
     return(
@@ -104,31 +66,26 @@ function MainBox(){
                 />
             </form>
             <div className={styles.buttonBox}>
-                <button onClick={onClick} className={styles.button} id="ing">진행중</button>
-                <button onClick={onClick} className={styles.button} id="today">오늘</button>
-                <button onClick={onClick} className={styles.button} id="done">완료</button>
+                <button onClick={onClick} className={show === -1 ? styles.buttonClicked : styles.button} name={"doing"}>진행중</button>
+                <button onClick={onClick} className={show === 0 ? styles.buttonClicked : styles.button} name={"all"}>오늘</button>
+                <button onClick={onClick} className={show === 1 ? styles.buttonClicked : styles.button} name={"done"}>완료</button>
             </div>
-            {   
-                r(todos, setLsLength, setChecked)
-            }
+            <div className={styles.todoBox}>
+                {
+                    ShowTodo(todos, handleDelete)
+                }
+            </div>
         </div>
     );
 }
 
-function r(todos, setLsLength, setChecked){
-    return(
-        <div className={styles.todoBox}>
+function ShowTodo(todos, handleDelete){
+    return(<>
             {todos.map(
-                todo => 
-                (<div key={todo} className={styles.todo + ' ' + 'todo'}>
-                    <Todo
-                        todo={todo}
-                        setLsLength={setLsLength}
-                        setChecked={setChecked}
-                    />
-                </div>)
-            )}
-        </div>
+            (current) =>{
+                return <Todo todo={current.todo} handleDelete={handleDelete} key={current.id}/>
+            })}
+        </>
     );
 }
 
--- plan-today/src/components/Todo.js
+++ plan-today/src/components/Todo.js
@@ -1,47 +1,50 @@
-import Button from "./Button";
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import styles from "../css/Todo.module.css";
 
-function Todo({todo, setLsLength, setChecked}){
+function Todo({todo, handleDelete}){
     const [mod, setMod] = useState(true);
     const [newInput, setNewInput] = useState("");
     const [newSubmit, setNewSubmit] = useState(todo);
+    const [checked, setChecked] = useState();
 
-    useEffect(()=>{
-        setNewInput(todo);
-    },[]);
-    useEffect(()=>{
-        if(newSubmit !== ""){
-            localStorage.setItem(newSubmit, newSubmit);
-        }
-    },[newSubmit])
     const modify = () =>{
         setMod(current => !current);
     }
+
     const onChange = (e) =>{
         setNewInput(e.target.value);
     }
+
     const onSubmit = (e) => {
         e.preventDefault();
         setNewSubmit(newInput);
-        localStorage.removeItem(newSubmit);
         modify();
     }
-    
+
+    const handleChecked = (e) => {
+        if(e.target.checked === true){
+            setChecked(true);
+        }
+        else{
+            setChecked(false);
+        }
+    }
+
     return(
         <>
             {
                 mod ?
                     <div className={styles.todo}>
-                        <Button tag={"input"} setChecked={setChecked}/>
+                        <input
+                            type="checkbox"
+                            onChange={handleChecked}
+                        />
                         <span className={styles.span}>
                             {newSubmit}
                         </span>
                         <div className={styles.buttonBox}>
-                            <button onClick={modify} className={styles.button}>
-                                수정
-                            </button>
-                            <Button name={"삭제"} todo={newSubmit} setLsLength={setLsLength}/>
+                            <button onClick={modify} className={styles.button}>수정</button>
+                            <button onClick={handleDelete} className={styles.button} name={newSubmit}>삭제</button>
                         </div>
                     </div>
                     :
@@ -49,9 +52,7 @@ function Todo({todo, setLsLength, setChecked}){
                         <form onSubmit={onSubmit} className={styles.form}>
                             <input onChange={onChange} className={styles.input} value={newInput}/>
                         </form>
-                        <button className={styles.button} onClick={modify}>
-                            x
-                        </button>
+                        <button className={styles.button} onClick={modify}>x</button>
                     </div>
             }
         </>

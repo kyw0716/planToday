f0d99ac803d4f9437455493de567acbfb7ac373e
kyw0716 <kyw0716@naver.com>
2022-10-18 19:04:28 +0900

    refactor: css => styled components

--- oneul/pages/index.js
+++ oneul/pages/index.js
@@ -1,16 +1,30 @@
-import styles from '../styles/Home.module.css'
-import Phrases from '../components/feature/Phrases'
-import Footer from '../components/layout/Footer'
-import dynamic from 'next/dynamic'
+import Phrases from "../components/feature/Phrases";
+import Footer from "../components/layout/Footer";
+import dynamic from "next/dynamic";
+import styled from "styled-components";
 
-const MainBox = dynamic(()=>import("../components/feature/mainBox/index"),{ssr : false})
+const MainBox = dynamic(() => import("../components/feature/mainBox/index"), {
+  ssr: false,
+});
+
+const Style = {
+  Container: styled.div`
+    width: 100%;
+    height: 120vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #efe4e0;
+  `,
+};
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Phrases/>
-      <MainBox/>
-      <Footer/>
-    </div>
-  )
+    <Style.Container>
+      <Phrases />
+      <MainBox />
+      <Footer />
+    </Style.Container>
+  );
 }
